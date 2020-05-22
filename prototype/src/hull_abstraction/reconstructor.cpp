2a48060d1eb5a6b4e7e4b175f9f9cedded2d73d6
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-05-22 10:25:21 -0700

    modify the prototype

--- prototype/src/hull_abstraction/reconstructor.cpp
+++ prototype/src/hull_abstraction/reconstructor.cpp
@@ -28,17 +28,16 @@ pcl::PolygonMesh hull_abstraction::Reconstructor::poissonReconstruction(pcl::Poi
 
     // Configurations
     poisson.setConfidence(false); //whether uses the magnitude of normals as confidence information. when false, all normals are normalised.
-    poisson.setDegree(2); //degree[1,5), the larger the value, the longer it takes.
-    poisson.setDepth(5);
-    poisson.setIsoDivide(5); //still dont know
+    poisson.setDegree(2);
+    poisson.setDepth(9);
+    poisson.setIsoDivide(10); //still dont know
     poisson.setManifold(false); //whether add the center of gravity of polygons. 
     poisson.setOutputPolygons(true);
-    poisson.setSamplesPerNode(20.0);
-    poisson.setScale(1.25);cloud_with_normals;
+    poisson.setSamplesPerNode(30);
+    poisson.setScale(1.0);
     poisson.setInputCloud(cloud_with_normals);
     poisson.performReconstruction(mesh);
     return mesh;
-
 }
 
 pcl::PolygonMesh hull_abstraction::Reconstructor::marchingCubesReconstruction(pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals)
@@ -49,7 +48,7 @@ pcl::PolygonMesh hull_abstraction::Reconstructor::marchingCubesReconstruction(pc
     tree->setInputCloud(cloud_with_normals);
     pcl::MarchingCubes<pcl::PointNormal>::Ptr marching_cubes(new pcl::MarchingCubesHoppe<pcl::PointNormal>);
     marching_cubes->setIsoLevel(0.0f);
-    marching_cubes->setGridResolution(50, 50, 50);
+    marching_cubes->setGridResolution(50, 15, 15);
     marching_cubes->setPercentageExtendGrid(0.0f);
     marching_cubes->setSearchMethod(tree);
     marching_cubes->setInputCloud(cloud_with_normals);
--- prototype/src/main.cpp
+++ prototype/src/main.cpp
@@ -10,30 +10,31 @@
 int main()
 {
     clock_t start, end;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals(new pcl::PointCloud<pcl::PointNormal>);
     pcl::PointCloud<pcl::PointNormal>::Ptr filtered_cloud_with_normals(new pcl::PointCloud<pcl::PointNormal>);
     pcl::PolygonMesh mesh1;
     pcl::PolygonMesh mesh2;
+    pcl::PolygonMesh mesh3;
+    pcl::PolygonMesh mesh4;
     //  Load original cloud and check if the file exists
-    // if (pcl::io::loadPCDFile<pcl::PointXYZ>("../point_cloud_data/16_5.pcd", *cloud1) == -1)
-    if (pcl::io::loadPLYFile<pcl::PointXYZ>("../point_cloud_data/bun_zipper.ply", *cloud1) == -1)
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>("../point_cloud_data/16_5.pcd", *cloud) == -1)
     {
         PCL_ERROR("Could not read file.\n");
         return(-1);
     }
-    std::cout << CLOCKS_PER_SEC << std::endl;
+
+    //std::cout << CLOCKS_PER_SEC << std::endl;
     hull_abstraction::Preprocessor pp;
     hull_abstraction::Reconstructor rc;
 
     // Down sampling
+
     //start = clock();
-    pp.statisticalFilter(cloud1, cloud);
-    // pp.voxelGridFilter(cloud, cloud);
+    //pp.statisticalFilter(cloud0, cloud);
     //end = clock();
-    //std::cout << "Time cost fordown sampling: " << (end - start) << " μs" << std::endl;
+    //std::cout << "Time cost for down sampling: " << (end - start) << " μs" << std::endl;
 
     // Moving least squares
     start = clock();
@@ -43,46 +44,65 @@ int main()
 
     //Normal estimation
     start = clock();
-    pp.appendNormalEstimation(cloud, cloud_with_normals);
+    pp.appendNormalEstimation(filtered_cloud, cloud_with_normals);
     end = clock();
-    std::cout << "Time cost for normal estimation (original cloud): " << (end - start)  << " μs" << std::endl;
+    std::cout << "Time cost for normal estimation: " << (end - start)  << " μs" << std::endl;
 
     //start = clock();
     //pp.appendNormalEstimation(filtered_cloud, filtered_cloud_with_normals);
     //end = clock();
     //std::cout << "Time cost for normal estimation (filtered cloud): " << (end - start)  << " μs" << std::endl;
-    // B-spline surface fitting
-    start = clock();
-    mesh1 = rc.marchingCubesReconstruction(cloud_with_normals);
-    end = clock();
-    std::cout << "Time cost for b-spline surface fitting (original cloud): " << (end - start)  << " μs" << std::endl;
-    
 
-    
     start = clock();
-    mesh2 = rc.marchingCubesReconstruction(filtered_cloud_with_normals);
+    mesh1 = rc.greedyTriangulation(cloud_with_normals);
     end = clock();
-    std::cout << "Time cost for b-spline surface fitting (moving least square): " << (end - start)  << " μs" << std::endl;
+    std::cout << "Time cost for greedy triangulation algorithm: " << (end - start)  << " μs" << std::endl;
+
+    start = clock();
+    mesh2 = rc.bsplineSurfaceFitting(filtered_cloud);
+    end = clock();
+    std::cout << "Time cost for b-spline surface fitting: " << (end - start)  << " μs" << std::endl;
+
+    start = clock();
+    mesh3 = rc.poissonReconstruction(cloud_with_normals);
+    end = clock();
+    std::cout << "Time cost for Poisson reconstruction: " << (end - start)  << " μs" << std::endl;
+
+    start = clock();
+    mesh4 = rc.marchingCubesReconstruction(cloud_with_normals);
+    end = clock();
+    std::cout << "Time cost for marching cubes algorithm: " << (end - start)  << " μs" << std::endl;
+
 
     // Display clouds
     boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
-    int v1(0), v2(0), v3(0), v4(0);
-    viewer->createViewPort(0.0, 0.5, 0.5, 1.0, v1);
-    viewer->createViewPort(0.5, 0.5, 1.0, 1.0, v2);
-    viewer->createViewPort(0.0, 0.0, 0.5, 0.5, v3);
-    viewer->createViewPort(0.5, 0.0, 1.0, 0.5, v4);
+    int v1(0), v2(0), v3(0), v4(0), v0(0);
+    viewer->createViewPort(0.0, 0.66, 1.0, 1.0, v0);
+    viewer->createViewPort(0.0, 0.33, 0.5, 0.66, v1);
+    viewer->createViewPort(0.5, 0.33, 1.0, 0.66, v2);
+    viewer->createViewPort(0.0, 0.0, 0.5, 0.33, v3);
+    viewer->createViewPort(0.5, 0.0, 1.0, 0.33, v4);
+    
+    // Set the background
+    viewer->setBackgroundColor(0, 0, 0,v0);
     viewer->setBackgroundColor(0, 0, 0,v1);
     viewer->setBackgroundColor(0, 0, 0,v2);
     viewer->setBackgroundColor(0, 0, 0,v3);
     viewer->setBackgroundColor(0, 0, 0,v4);
-    viewer->addText("Original cloud", 10, 10, "text1", v1);
-    viewer->addText("Filtered cloud", 10, 10, "text2", v2);
-    viewer->addText("Meshes of original cloud", 10, 10, "text3", v3);
-    viewer->addText("Meshes of filtered cloud", 10, 10, "text4", v4);
-    viewer->addPointCloud(cloud, "cloud1", v1);
-    viewer->addPointCloud(filtered_cloud, "cloud2", v2);
-    viewer->addPolygonMesh(mesh1, "mesh1", v3);
-    viewer->addPolygonMesh(mesh2, "mesh2", v4);
+
+    viewer->addText("Point Cloud", 10, 10, "text0", v0);
+    viewer->addText("Greedy Triangulation", 10, 10, "text1", v1);
+    viewer->addText("B-Spline Surface Fitting", 10, 10, "text2", v2);
+    viewer->addText("Poisson Reconstruction", 10, 10, "text3", v3);
+    viewer->addText("Marching Cubes Algorithm", 10, 10, "text4", v4);
+
+    viewer->addPointCloud(filtered_cloud, "cloud0", v0);
+  
+    viewer->addPolygonMesh(mesh1, "mesh1", v1);
+    viewer->addPolygonMesh(mesh2, "mesh2", v2);
+
+    viewer->addPolygonMesh(mesh3, "mesh3", v3);
+    viewer->addPolygonMesh(mesh4, "mesh4", v4);
     // viewer->setRepresentationToWireframeForAllActors();
     viewer->initCameraParameters();
     while (!viewer->wasStopped())
