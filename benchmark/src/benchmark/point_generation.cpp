28dd3e87949ba13939f7fb7d43e1ad887b7dde15
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-06-19 15:35:07 +0200

    update PointGenerator class

--- benchmark/src/benchmark/point_generation.cpp
+++ benchmark/src/benchmark/point_generation.cpp
@@ -32,7 +32,16 @@ pcl::PointCloud<pcl::PointNormal>::Ptr benchmark::PointGenerator::randomlySampli
     size_t triangle_size = mesh.polygons.size();
     for (size_t i = 0; i < triangle_size; i++)
     {
-        mesh_area += benchmark::calculateArea(mesh.polygons[i]);
+        std::vector<std::vector<double>> triangle; 
+        for (size_t j = 0; j < 3; j++)
+        {
+            triangle.push_back({
+                mesh_cloud->points[mesh.polygons[i].vertices[j]].x, 
+                mesh_cloud->points[mesh.polygons[i].vertices[j]].y,
+                mesh_cloud->points[mesh.polygons[i].vertices[j]].z
+            });
+        }
+        mesh_area += benchmark::calculateArea(triangle);
         area_mapping.push_back(mesh_area);
     }
 
